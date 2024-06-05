454d7ec225269063f2b6dcd76f0d0899863abe64
arvindkaphley <88549860+arvindkaphley@users.noreply.github.com>
2024-06-05 12:10:12 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,79 @@
-const about=document.querySelector('#about')
-const contact=document.querySelector('#contact')
-const aboutContent=document.querySelector('#about-content')
-const contactContent=document.querySelector('#contact-content')
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
+const aboutContent = document.querySelector('#about-content');
+const contactContent = document.querySelector('#contact-content');
 
-let aboutBox=null
-let contactBox=null
+let aboutBox = null;
+let contactBox = null;
 
-// function typeWriter(element, text, delay = 100) {
-//     element.innerHTML = '';
-//     let i = 0;
-//     function type() {
-//         if (i < text.length) {
-//             element.innerHTML += text.charAt(i);
-//             i++;
-//             setTimeout(type, delay);
-//         }
-//     }
-//     type();
-// }
+function getViewportSize() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+}
 
-about.addEventListener('click',()=>{
-    if(aboutBox){
-        aboutBox.close()
-        aboutBox=null
-    }else{
-    aboutBox = new WinBox({
-        title : 'About Me',
-        width : '400px',
-        height : '400px',
-        top : 50,
-        right : 50,
-        bottom : 50,
-        left : 50,
-        mount: aboutContent,
-        onfocus: function(){
-            this.setBackground('#00aa00')
-        },
-        onblur: function(){
-            this.setBackground('#777')
-        },
-        onclose: function(){
-            aboutBox=null
-        }
-    })
-    // typeWriter(aboutContent,aboutContent.textContent)
+about.addEventListener('click', () => {
+    if (aboutBox) {
+        aboutBox.close();
+        aboutBox = null;
+    } else {
+        const { width, height } = getViewportSize();
+        aboutBox = new WinBox({
+            title: 'About Me',
+            width: width > 600 ? '400px' : '80%',
+            height: height > 600 ? '400px' : '60%',
+            top: 50,
+            right: width > 600 ? 50 : '10%',
+            bottom: 50,
+            left: width > 600 ? 50 : '10%',
+            mount: aboutContent,
+            onfocus: function() {
+                this.setBackground('#00aa00');
+            },
+            onblur: function() {
+                this.setBackground('#777');
+            },
+            onclose: function() {
+                aboutBox = null;
+            }
+        });
     }
-})
-contact.addEventListener('click',()=>{
-    if(contactBox){
-        contactBox.close()
-        contactBox=null
-    }else{
-    contactBox = new WinBox({
-        title : 'Contact Me',
-        width : '400px',
-        height : '400px',
-        top : 150,
-        right : 50,
-        bottom : 50,
-        left : 250,
-        mount: contactContent,
-        onfocus: function(){
-            this.setBackground('#00aa00')
-        },
-        onblur: function(){
-            this.setBackground('#777')
-        },
-        onclose: function(){
-            contactBox=null
-        }
-    })
-    // typeWriter(contactContent,contactContent.textContent)
+});
+
+contact.addEventListener('click', () => {
+    if (contactBox) {
+        contactBox.close();
+        contactBox = null;
+    } else {
+        const { width, height } = getViewportSize();
+        contactBox = new WinBox({
+            title: 'Contact Me',
+            width: width > 600 ? '400px' : '80%',
+            height: height > 600 ? '400px' : '60%',
+            top: 150,
+            right: width > 600 ? 50 : '10%',
+            bottom: 50,
+            left: width > 600 ? 250 : '10%',
+            mount: contactContent,
+            onfocus: function() {
+                this.setBackground('#00aa00');
+            },
+            onblur: function() {
+                this.setBackground('#777');
+            },
+            onclose: function() {
+                contactBox = null;
+            }
+        });
     }
-})
+});
+
+window.addEventListener('resize', () => {
+    if (aboutBox) {
+        aboutBox.move(50, 50).resize('400px', '400px');
+    }
+    if (contactBox) {
+        contactBox.move(150, 50).resize('400px', '400px');
+    }
+});
